c79dbc802f41b14bca288154168b5841ea11806c
Gourav Mohanty <gouravmohanty2001@gmail.com>
2023-11-10 11:09:04 +0530

    feat: Implemented audio extraction view

--- vidyo/vidyo_processing/views.py
+++ vidyo/vidyo_processing/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from moviepy.editor import VideoFileClip
+from rest_framework import views, status
+from rest_framework.response import Response
+from .models import Video
+from .serializers import VideoSerializer
 
-# Create your views here.
+class ExtractAudioView(views.APIView):
+    def post(self, request, format=None):
+        # Extract audio from video
+        video_file = request.FILES['file']
+        video = VideoFileClip(video_file.temporary_file_path())
+        audio = video.audio
+
+        # Define the path to save the audio file (for a Mac)
+        audio_file = "/Users/gouravmohanty/Desktop/audio.mp3"  # Update with your path
+
+        audio.write_audiofile(audio_file)
+
+        # Save video info to DB
+        video_info = Video(user=request.user.username)
+        video_info.save()
+
+        return Response(VideoSerializer(video_info).data, status=status.HTTP_200_OK)
