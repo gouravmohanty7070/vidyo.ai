829f2a9219b0c84d6ef7bc97387b9775576ae9e4
Gourav Mohanty <gouravmohanty2001@gmail.com>
2023-11-10 11:34:48 +0530

    feat: Implemented video watermarking view

--- vidyo/vidyo/urls.py
+++ vidyo/vidyo/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from vidyo_processing.views import ExtractAudioView
+from vidyo_processing.views import ExtractAudioView, WatermarkVideoView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('extract-audio/', ExtractAudioView.as_view()),
+    path('watermark-video/', WatermarkVideoView.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- vidyo/vidyo_processing/views.py
+++ vidyo/vidyo_processing/views.py
@@ -1,8 +1,10 @@
-from moviepy.editor import VideoFileClip
+from moviepy.editor import VideoFileClip, CompositeVideoClip, ImageClip
+from django.conf import settings
 from rest_framework import views, status
 from rest_framework.response import Response
 from .models import Video
 from .serializers import VideoSerializer
+
 
 class ExtractAudioView(views.APIView):
     def post(self, request, format=None):
@@ -18,6 +20,34 @@ class ExtractAudioView(views.APIView):
 
         # Save video info to DB
         video_info = Video(user=request.user.username)
+        video_info.save()
+
+        return Response(VideoSerializer(video_info).data, status=status.HTTP_200_OK)
+    
+
+class WatermarkVideoView(views.APIView):
+    def post(self, request, format=None):
+        video_file = request.FILES.get('video')
+        watermark_image = request.FILES.get('watermark')
+        position = request.data.get('position', 'center')  # Default position
+
+        if not video_file or not watermark_image:
+            return Response({"message": "Video and watermark image are required."}, 
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        video_clip = VideoFileClip(video_file.temporary_file_path())
+        watermark_clip = ImageClip(watermark_image.temporary_file_path(), duration=video_clip.duration)
+
+        # Positioning the watermark
+        watermark_clip = watermark_clip.set_position(position)
+
+        # Overlay watermark on video
+        final_clip = CompositeVideoClip([video_clip, watermark_clip])
+        output_path = f"{settings.MEDIA_ROOT}/watermarked/{video_file.name}"
+        final_clip.write_videofile(output_path, codec="libx264", audio_codec="aac")
+
+        # Save video info to DB
+        video_info = Video(user=request.user.username, watermark_image=watermark_image)
         video_info.save()
 
         return Response(VideoSerializer(video_info).data, status=status.HTTP_200_OK)
