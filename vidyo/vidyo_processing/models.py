a60a679e0de9069d070a638f7611b2c8e9aafde7
Gourav Mohanty <gouravmohanty2001@gmail.com>
2023-11-10 15:52:47 +0530

    fix: FFmpeg used

--- vidyo/vidyo_processing/models.py
+++ vidyo/vidyo_processing/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Video(models.Model):
     user = models.CharField(max_length=100)
     upload_time = models.DateTimeField(auto_now_add=True)
-    watermark_image = models.ImageField(upload_to='watermarks/', blank=True, null=True)  # Image watermark
-    watermark_position = models.CharField(max_length=50, blank=True)  # Position of the watermark
+    watermark_image = models.ImageField(upload_to='watermarks/', blank=True, null=True)  
+    watermark_position = models.CharField(max_length=50, blank=True) 
--- vidyo/vidyo_processing/views.py
+++ vidyo/vidyo_processing/views.py
@@ -1,25 +1,24 @@
-from moviepy.editor import VideoFileClip, CompositeVideoClip, ImageClip
 from django.conf import settings
 from rest_framework import views, status
 from rest_framework.response import Response
 from .models import Video
 from .serializers import VideoSerializer
 
+import subprocess,os
+
+
 
 class ExtractAudioView(views.APIView):
     def post(self, request, format=None):
-        # Extract audio from video
         video_file = request.FILES['file']
-        video = VideoFileClip(video_file.temporary_file_path())
-        audio = video.audio
+        output_filename = video_file.name.split('.')[0] + '.mp3'
+        output_path = os.path.join(settings.MEDIA_ROOT, output_filename)
 
-        # Define the path to save the audio file (for a Mac)
-        audio_file = "/Users/gouravmohanty/Desktop/audio.mp3"  # Update with your path
+        command = ['ffmpeg', '-i', video_file.temporary_file_path(), '-q:a', '0', '-map', 'a', output_path]
+        subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        
 
-        audio.write_audiofile(audio_file)
-
-        # Save video info to DB
-        video_info = Video(user=request.user.username)
+        video_info = Video(user=request.user.username if request.user.is_authenticated else 'Anonymous')
         video_info.save()
 
         return Response(VideoSerializer(video_info).data, status=status.HTTP_200_OK)
@@ -31,23 +30,41 @@ class WatermarkVideoView(views.APIView):
         watermark_image = request.FILES.get('watermark')
         position = request.data.get('position', 'center')  # Default position
 
-        if not video_file or not watermark_image:
-            return Response({"message": "Video and watermark image are required."}, 
-                            status=status.HTTP_400_BAD_REQUEST)
 
-        video_clip = VideoFileClip(video_file.temporary_file_path())
-        watermark_clip = ImageClip(watermark_image.temporary_file_path(), duration=video_clip.duration)
+        output_filename = video_file.name.split('.')[0] + '_watermarked.mp4'  # Modified filename
+        output_path = os.path.join(settings.MEDIA_ROOT, output_filename)
 
-        # Positioning the watermark
-        watermark_clip = watermark_clip.set_position(position)
 
-        # Overlay watermark on video
-        final_clip = CompositeVideoClip([video_clip, watermark_clip])
-        output_path = f"/Users/gouravmohanty/Desktop/{video_file.name}"
-        final_clip.write_videofile(output_path, codec="libx264", audio_codec="aac")
+        command = [
+            'ffmpeg', '-i', video_file.temporary_file_path(), '-i', watermark_image.temporary_file_path(),
+            '-filter_complex', 'overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2', 
+            '-codec:a', 'copy', output_path
+        ]
+        subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
         # Save video info to DB
         video_info = Video(user=request.user.username, watermark_image=watermark_image)
         video_info.save()
 
-        return Response(VideoSerializer(video_info).data, status=status.HTTP_200_OK)
+        return Response(VideoSerializer(video_info).data, status=status.HTTP_200_OK)      
+
+        # if not video_file or not watermark_image:
+        #     return Response({"message": "Video and watermark image are required."}, 
+        #                     status=status.HTTP_400_BAD_REQUEST)
+
+        # video_clip = VideoFileClip(video_file.temporary_file_path())
+        # watermark_clip = ImageClip(watermark_image.temporary_file_path(), duration=video_clip.duration)
+
+        # # Positioning the watermark
+        # watermark_clip = watermark_clip.set_position(position)
+
+        # # Overlay watermark on video
+        # final_clip = CompositeVideoClip([video_clip, watermark_clip])
+        # output_path = f"/Users/gouravmohanty/Desktop/{video_file.name}"
+        # final_clip.write_videofile(output_path, codec="libx264", audio_codec="aac")
+
+        # # Save video info to DB
+        # video_info = Video(user=request.user.username, watermark_image=watermark_image)
+        # video_info.save()
+
+        # return Response(VideoSerializer(video_info).data, status=status.HTTP_200_OK)
